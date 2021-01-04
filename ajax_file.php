05f3e9476a658cc9a8e1ffd8639d19fbbee96b9f
gabri <gabri@DESKTOP-JRMA0U1>
2021-01-03 23:02:40 -0600

    primer commit para instalar en un servidor publico heruko

---
+++ ajax_file.php
@@ -0,0 +1,33 @@
+<?php
+
+$strJsonFileContents = file_get_contents("nombresCompletosJson.json");
+
+$array = json_decode($strJsonFileContents, true);
+
+$nombres = $array['results'];
+
+$respuesta = array();
+for ($index = 0; $index < count($nombres); $index++) {
+    array_push($respuesta, [
+        $nombres[$index]['Name'],
+        $nombres[$index]['Gender']
+    ]);
+}
+$js = json_encode(['data' => $respuesta]);
+
+echo $js;
+
+function generarScript() {
+    $strJsonFileContents = file_get_contents("nombresCompletosJson.json");
+
+    $array = json_decode($strJsonFileContents, true);
+
+    $nombres = $array['results'];
+    
+    echo "<br> insert into ??? (nombre,genero) values <br>";
+    for ($index = 0; $index < count($nombres); $index++) {
+        echo '(' . $nombres[$index]['Name'] . ',' . $nombres[$index]['Gender'] . ') , <br>';
+    }
+}
+
+//echo generarScript();
