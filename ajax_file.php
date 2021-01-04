d2875ec74a7421bb9efcc4bf0e36da088f936c23
gabri <gabri@DESKTOP-JRMA0U1>
2021-01-03 23:20:02 -0600

    aligerando el procesamiento

--- ajax_file.php
+++ ajax_file.php
@@ -1,14 +1,15 @@
 <?php
+// el archivo original es muy grande para que el servidor de prueba lo pueda manejar...
+//$strJsonFileContents = file_get_contents("nombresCompletosJson.json");
 
-$strJsonFileContents = file_get_contents("nombresCompletosJson.json");
-
+$strJsonFileContents = file_get_contents("nombresCompletosJson_reducido.json");
 $array = json_decode($strJsonFileContents, true);
 
 $nombres = $array['results'];
 
 $respuesta = array();
 // se resta 249000 dado que el servidor no puede procesar tantos datos...
-for ($index = 0; $index < count($nombres)-249900; $index++) {
+for ($index = 0; $index < count($nombres); $index++) {
     array_push($respuesta, [
         $nombres[$index]['Name'],
         $nombres[$index]['Gender']
